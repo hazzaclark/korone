0c345274ed56fe6b85f23284a54c3d8c6046367c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-01-26 20:30:16 +0000

    demonstrating with a 68k bin

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,22 @@ mod lib;
 
 // SYSTEM INCLUDES
 
+use std::fs::File;
+use std::io::{self, BufReader};
+use lib::{SOURCE_ENDIAN, ENDIANESS, ENDIAN_READ_ERR};
 use std::env as ENV;
 use std::process as PROC;
+
+// EXAMPLE DEMONSTRATION OF PARSING A BINARY FILE FOR COMPILED ASSEMBLY CODE
+
+#[repr(C)]
+#[derive(Debug)]
+struct INSTR 
+{
+    OPCODE: u16, 
+    OPERAND: u16,
+}
+
 
 fn main()
 {
