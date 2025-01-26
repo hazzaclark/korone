48233577e4b62efafa8bb355c75909bb2ecd3a7d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-01-26 20:57:53 +0000

    removed unused methods

--- src/lib.rs
+++ src/lib.rs
@@ -7,85 +7,17 @@
 
 // SYSTEM INCLUDES
 
-use std::mem;
-use std::marker::PhantomData;
-use std::convert::TryInto;
-use std::io::{Read, Write};
-use std::io::{Seek, SeekFrom};
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-// STATICALLY AVAILABLE TYPE LEVEL TRAITS FOR ASSERTING
-// DIFFERENT GATE CONDITIONS
+use std::io::{Read};
 
-mod TYPE_TRAIT
-{
-    pub trait NOT { type Output; }
-
-    impl NOT for bool { type Output = bool; }
-
-    pub trait Conjunction<T: = bool> { type Output; }
-
-    pub trait Disconjunction<T: = bool> { type Output; }
-
-    pub trait IsAnyOf<T> { const VALUE: bool; }
-
-    pub trait TRAIT_CONT 
-    {
-        fn HAS_PUSH_BACK() -> bool;
-        fn HAS_SIZE() -> bool;
-        fn HAS_RESIZE() -> bool;
-        fn HAS_DATA() -> bool;
-    }
-}
-
-// ENDIANESS DEFINES AND DIRECTIVES
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ENDIANESS
-{
-    Little,
-    Big,
-}
-
+#[derive(Debug)]
 pub enum ENDIAN_READ_ERR 
 {
     IoError(std::io::Error),
     BufferTooSmall,
     ConversionError,
-}
-
-pub trait ENDIAN_BYTE_SWAP
-{
-    fn SWAP_BYTES(self) -> Self;
-}
-
-// FULL FLEDGED ENDIAN UTILITIES USED FOR CONVERSION AND SWAPPING
-// THE FOLLOWING WILL TAKE A LOOK INTO THE VARIOUS TYPES ENCOMPASSING SIGNED 
-// AND UNSIGNED VALUES AND HANDLE THEIR RESPECTIVE LENGTHS
-
-pub struct ENDIAN_UTIL;
-impl ENDIAN_UTIL
-{
-    pub fn B_TYPE_SWAP<T: ENDIAN_BYTE_SWAP>(VALUE: T) -> T
-    {
-        VALUE.SWAP_BYTES()
-    }
-
-    pub fn SELECT_UNSIGNED<T>(SIZE: usize) -> Option<T>
-    where
-        T: From<u8> + From<u16> + From<u32> + From<u64>
-    {
-
-        // MATCH SIZE BASED ON AMOUNT OF BYTES BEING ALLOCATED FOR EACH DATA TYPE
-
-        match SIZE
-        {
-            1 => Some(T::from(0u8)),
-            2 => Some(T::from(0u16)),
-            4 => Some(T::from(0u32)),
-            8 => Some(T::from(0u64)),
-            _ => None
-        }
-    }
 }
 
 // CREATE A STD DISPLAY OUTPUT FOR ANY AND ALL OF THE POSSIBLE OPTIONS
@@ -115,7 +47,7 @@ impl SOURCE_ENDIAN
     // THE DEFINING FEATURE IS THAT THIS READ SCHEMA MUST IMPLEMENT THE READ TRAIT
     // WHICH ENCOMPASSES A FILE, BUFFER, OR STREAM
 
-    pub fn READ<R: Read, T>(mut READER: R) -> Result<T, ENDIAN_READ_ERR>
+    pub fn READ<R: Read, T>(READER: &mut R) -> Result<T, ENDIAN_READ_ERR>
     where
         T: Default + Copy
     {
@@ -137,45 +69,5 @@ impl SOURCE_ENDIAN
         }).map_err(ENDIAN_READ_ERR::IoError)?;
 
         Ok(VALUE)
-    }
-
-    // NOW THE SAME BUT REVERSE
-
-    pub fn READ_INVERSE<R: Read, T>(mut READER: R) -> Result<T, ENDIAN_READ_ERR>
-    where
-        T: Default + Copy
-    {
-        let mut VALUE = T::default();
-
-        // USE THE READ_EXACT METHOD IN ORDE3R TO EXTRAPOLATE THE SIZE OF
-        // BYTES INTO VALUE
-
-        // I LEARNT THAT USING THE FROM RAW PARTS MUTABLE DIRECTIVE ALLOWS
-        // FOR THE MUTABLE ALLOCATION OF MEMORY TO THAT POINTER
-
-        READER.read_exact(unsafe 
-        {
-            std::slice::from_raw_parts_mut
-            (
-                &mut VALUE as *mut T as *mut u8,
-                std::mem::size_of::<T>(),
-            )
-        }).map_err(ENDIAN_READ_ERR::IoError)?;
-
-        Ok(VALUE.SWAP_BYTES)
-    }
-
-    pub fn WRITE<W: Write, T>(mut WRITER: W, VALUE: T) -> Result<(), ENDIAN_READ_ERR>
-    where
-        T: Copy
-    {
-        WRITER.write_all(unsafe
-        {
-            std::slice::from_raw_parts
-            (
-                &VALUE as *const T as *const u8,
-                std::mem::size_of::<T>()
-            )
-        })..map_err(ENDIAN_READ_ERR::IoError)
     }
 }
