0be2132539ba47b3c619d80d1e1f9bb8e1715849
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-01-26 20:57:35 +0000

    added basic opcode and operand reader

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@
 
 // LIGHTWEIGHT ENDIANESS PARSER AND LEXER IN RUST
 
+// ADD THESE LINES BECAUSE THE COMPILER WAS SCREAMING AT ME
+// HEAVEN FORBID YOU HAVE A CODING STYLE ¯\_(ツ)_/¯
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 mod lib;
 
 // SYSTEM INCLUDES
 
 use std::fs::File;
-use std::io::{self, BufReader};
-use lib::{SOURCE_ENDIAN, ENDIANESS, ENDIAN_READ_ERR};
+use std::io::{Read, BufReader};
+use crate::lib::{SOURCE_ENDIAN, ENDIAN_READ_ERR};
 use std::env as ENV;
 use std::process as PROC;
 
@@ -16,12 +22,54 @@ use std::process as PROC;
 
 #[repr(C)]
 #[derive(Debug)]
-struct INSTR 
+pub struct INSTR 
 {
     OPCODE: u16, 
     OPERAND: u16,
 }
 
+// FUNCTION TO PARSE BINARY FILE
+
+pub fn PARSE_BINARY<R: Read>(READER: &mut R) -> Result<Vec<INSTR>, ENDIAN_READ_ERR> 
+{
+    let mut INSTRUCTIONS = Vec::new();
+    
+    loop 
+    {
+        let OPCODE = match SOURCE_ENDIAN::READ::<R, u16>(READER) 
+        {
+            Ok(OP) => OP,
+            Err(_) => break,
+        };
+        
+        let OPERAND = match SOURCE_ENDIAN::READ::<R, u16>(READER) 
+        {
+            Ok(OPER) => OPER,
+            Err(_) => break,
+        };
+        
+        INSTRUCTIONS.push(INSTR { OPCODE, OPERAND });
+    }
+    
+    Ok(INSTRUCTIONS)
+}
+
+// FUNCTION TO PROCESS A BINARY FILE
+
+fn PROC_FILE(F_PATH: &str) -> Result<(), Box<dyn std::error::Error>> 
+{
+    let FILE = File::open(F_PATH)?;
+    let mut READER = BufReader::new(FILE);
+
+    let INSTRUCTIONS = PARSE_BINARY(&mut READER)?;
+
+    for (i, INSTR) in INSTRUCTIONS.iter().enumerate() 
+    {
+        println!("Instruction {}: {:?}", i, INSTR);
+    }
+
+    Ok(())
+}
 
 fn main()
 {
@@ -31,11 +79,18 @@ fn main()
     {
         eprintln!
         (
-            "Usage: {} <INPUT_FILE> <OUTPUT_FMT>\n", args[0]
+            "Usage: {} <INPUT_FILE>\n", args[0]
         );
 
         PROC::exit(1);
     }
 
     let INPUT = &args[1];
+
+    if let Err(err) = PROC_FILE(INPUT)
+    {
+        eprintln!("Error processing Input File {}", err);
+        PROC::exit(1);
+    }
 }
+
