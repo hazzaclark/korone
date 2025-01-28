2d46d52eb2be01c8fcfc884e6240a8b7b7c54384
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-01-28 14:06:56 +0000

    Update endian.rs

--- src/endian.rs
+++ src/endian.rs
@@ -10,7 +10,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::io::{Read};
+use std::io::Read;
 
 #[derive(Debug)]
 pub enum ENDIAN_READ_ERR 
@@ -30,73 +30,81 @@ impl std::fmt::Display for ENDIAN_READ_ERR
         match self 
         {
             ENDIAN_READ_ERR::IoError(err) => write!(f, "IO Error: {}", err),
-            ENDIAN_READ_ERR::BufferTooSmall => write!(f, "Buffer is too small"),
-            ENDIAN_READ_ERR::ConversionError => write!(f, "Conversion error"),
-            ENDIAN_READ_ERR::EndOfFile => write!(f, "End of file reached"),
+            ENDIAN_READ_ERR::BufferTooSmall => write!(f, "Buffer is too small to process the data."),
+            ENDIAN_READ_ERR::ConversionError => write!(f, "Error during byte conversion."),
+            ENDIAN_READ_ERR::EndOfFile => write!(f, "Reached end of file."),
         }
     }
 }
 
 impl std::error::Error for ENDIAN_READ_ERR {}
 
-
-// MAIN ENCOMPASSING READING AND WRITING SCHEMA
-
 pub struct SOURCE_ENDIAN;
-impl SOURCE_ENDIAN
+impl SOURCE_ENDIAN 
 {
     // DEFINE A GENERIC METHOD TO READ A SPECIFIED VALUE FROM THE READER
     // THE DEFINING FEATURE IS THAT THIS READ SCHEMA MUST IMPLEMENT THE READ TRAIT
     // WHICH ENCOMPASSES A FILE, BUFFER, OR STREAM
 
-    // NOWE WE CAN READ SPECIFIC ENDIAN LEVELS
 
     pub fn READ_LE<R: Read, T>(READER: &mut R) -> Result<T, ENDIAN_READ_ERR>
     where
-        T: Default + Copy + std::fmt::Debug,
+        T: Default + Copy + std::fmt::Debug + Sized,
+    {
+        let SIZE = std::mem::size_of::<T>();
+        let mut BUFFER = vec![0u8; SIZE];
+
+        match READER.read_exact(&mut BUFFER) 
         {
-            let mut VALUE = T::default();
-            let SIZE = std::mem::size_of::<T>();
-            let mut BUFFER = vec![0u8; SIZE];
-
-            READER.read_exact(&mut BUFFER).map_err(ENDIAN_READ_ERR::IoError)?;
-
-            // CONVERT LE BYTES TO THE TARGET
-
-            for(i, BYTE) in BUFFER.iter().enumerate()
+            Ok(_) => 
             {
-                unsafe
+                let mut VALUE = T::default();
+                for (i, &BYTE) in BUFFER.iter().enumerate() 
                 {
-                    let VALUE_PTR = (&mut VALUE as *mut T as *mut u8).add(i);
-                    *VALUE_PTR = *BYTE;
+                    unsafe 
+                    {
+                        let VALUE_PTR = (&mut VALUE as *mut T as *mut u8).add(i);
+                        *VALUE_PTR = BYTE;
+                    }
                 }
+                Ok(VALUE)
             }
-
-            Ok(VALUE)
+            Err(err) if err.kind() == std::io::ErrorKind::UnexpectedEof => 
+            {
+                Err(ENDIAN_READ_ERR::EndOfFile)
+            }
+            
+            Err(err) => Err(ENDIAN_READ_ERR::IoError(err)),
         }
-
+    }
 
     pub fn READ_BE<R: Read, T>(READER: &mut R) -> Result<T, ENDIAN_READ_ERR>
     where
-        T: Default + Copy + std::fmt::Debug,
+        T: Default + Copy + std::fmt::Debug + Sized,
+    {
+        let SIZE = std::mem::size_of::<T>();
+        let mut BUFFER = vec![0u8; SIZE];
+
+        match READER.read_exact(&mut BUFFER) 
         {
-            let mut VALUE = T::default();
-            let SIZE = std::mem::size_of::<T>();
-            let mut BUFFER = vec![0u8; SIZE];
-    
-            READER.read_exact(&mut BUFFER).map_err(ENDIAN_READ_ERR::IoError)?;
-    
-            // CONVERT LE BYTES TO THE TARGET
-    
-            for(i, BYTE) in BUFFER.iter().rev().enumerate()
+            Ok(_) => 
             {
-                unsafe
+                let mut VALUE = T::default();
+                for (i, &BYTE) in BUFFER.iter().rev().enumerate() 
                 {
-                    let VALUE_PTR = (&mut VALUE as *mut T as *mut u8).add(i);
-                    *VALUE_PTR = *BYTE;
+                    unsafe 
+                    {
+                        let VALUE_PTR = (&mut VALUE as *mut T as *mut u8).add(i);
+                        *VALUE_PTR = BYTE;
+                    }
                 }
+                Ok(VALUE)
             }
-    
-            Ok(VALUE)
+            Err(err) if err.kind() == std::io::ErrorKind::UnexpectedEof => 
+            {
+                Err(ENDIAN_READ_ERR::EndOfFile)
+            }
+            Err(err) => Err(ENDIAN_READ_ERR::IoError(err)),
         }
+    }
 }
